b8310b661ad5684ca04097d1326b57203ce6de19
bennyhawk <reubennellissery@gmail.com>
2023-10-14 07:32:53 -0400

    Added the webhook api

--- functions/main.py
+++ functions/main.py
@@ -7,7 +7,6 @@ from firebase_functions import https_fn
 from firebase_admin import initialize_app, firestore
 from firebase_functions import options
 
-
 from api import retrieve_user_data
 
 options.set_global_options(max_instances=10)
@@ -15,7 +14,27 @@ options.set_global_options(max_instances=10)
 app = initialize_app()
 
 
+def webhook_processor(req: https_fn.Request) -> https_fn.Response:
+    resp = {
+        "fulfillmentResponse": {{
+            "messages": [
+                {
+                    "text": {
+                        [
+                            "This is string 1",
+                            "This is string 2",
+                        ]
+                    },
+                }
+            ],
+        }}
+    }
+    return https_fn.Response(response=json.dumps(resp), content_type='application/json')
+
+
 @https_fn.on_request()
 def on_request_example(req: https_fn.Request) -> https_fn.Response:
     if req.path == '/transactions':
         return retrieve_user_data(req)
+    if req.path == '/webhook':
+        return webhook_processor(req)
